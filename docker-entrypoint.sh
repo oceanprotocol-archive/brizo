7bb0ea38c7aee42619d872790c8e80a293a78278
Javier Cortejoso <javier.cortejoso@gmail.com>
2019-01-04 12:16:54 +0100

    Override only local contracts (spree and development)

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -9,7 +9,8 @@ if [ "${LOCAL_CONTRACTS}" = "true" ]; then
   done
 fi
 
-/bin/cp -rp /usr/local/keeper-contracts/* /usr/local/artifacts/
+/bin/cp -rp /usr/local/keeper-contracts/*.spree.json /usr/local/artifacts/ || true
+/bin/cp -rp /usr/local/keeper-contracts/*.development.json /usr/local/artifacts/ || true
 
 gunicorn -b ${BRIZO_URL#*://} -w ${BRIZO_WORKERS} brizo.run:app
 tail -f /dev/null
