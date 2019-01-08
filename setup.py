6de8c5252960fd6274c7e7e9f24455a247de3666
Javier Cortejoso <javier.cortejoso@gmail.com>
2019-01-08 11:28:46 +0100

    Bump version: 0.1.3 → 0.1.4

--- setup.py
+++ setup.py
@@ -41,6 +41,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/oceanprotocol/brizo',
-    version='0.1.3',
+    version='0.1.4',
     zip_safe=False,
 )
