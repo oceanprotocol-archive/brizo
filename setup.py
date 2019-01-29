ed6ba4ada63c5ff82121f3580e46b4cef752cc88
eruizgar91 <enrique@oceanprotocol.com>
2019-01-29 11:56:56 +0100

    Add missed gunicorn dependency

--- setup.py
+++ setup.py
@@ -21,6 +21,7 @@ install_requirements = [
     'Flask-RESTful==0.3.6',
     'flask-swagger==0.2.13',
     'flask-swagger-ui==3.6.0',
+    'gunicorn==19.9.0',
     'osmosis-azure-driver==0.0.4',
     'osmosis-aws-driver==0.0.2',
     'osmosis-driver-interface==0.0.6',
