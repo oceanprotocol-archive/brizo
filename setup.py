807fb94039ae8eef135ff7b81e30bb7f72b28b7f
eruizgar91 <enrique@oceanprotocol.com>
2019-02-01 17:22:30 +0100

    Bump version: 0.1.8 → 0.1.9

--- setup.py
+++ setup.py
@@ -79,6 +79,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/oceanprotocol/brizo',
-    version='0.1.8',
+    version='0.1.9',
     zip_safe=False,
 )
