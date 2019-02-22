3a1107292eb9305b91cdb709768391b82fb28604
eruizgar91 <enrique@oceanprotocol.com>
2019-02-22 16:49:36 +0100

    Fix swagger doc and remove unnecessary code in the config setting.

--- brizo/myapp.py
+++ brizo/myapp.py
@@ -11,10 +11,4 @@ CORS(app)
 if 'CONFIG_FILE' in os.environ and os.environ['CONFIG_FILE']:
     app.config['CONFIG_FILE'] = os.environ['CONFIG_FILE']
 else:
-    try:
-        app.config['CONFIG_FILE'] = 'config.ini'
-    except Exception as e:
-        logging.error(
-            'A config file must be set in the environment variable "CONFIG_FILE" or in config.ini')
-        logging.error(e)
-        sys.exit(1)
+    app.config['CONFIG_FILE'] = 'config.ini'
