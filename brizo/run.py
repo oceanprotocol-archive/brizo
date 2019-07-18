4ae24c86c8f02cb0c0e8a101e85add8b53543337
eruizgar91 <enrique@oceanprotocol.com>
2019-07-18 10:24:39 +0200

    Return provider address in brizo info

--- brizo/run.py
+++ brizo/run.py
@@ -13,6 +13,9 @@ from brizo.constants import BaseURLs, ConfigSections, Metadata
 from brizo.myapp import app
 from brizo.routes import services
 
+config = Config(filename=app.config['CONFIG_FILE'])
+brizo_url = config.get(ConfigSections.RESOURCES, 'brizo.url')
+
 
 def get_version():
     conf = configparser.ConfigParser()
@@ -26,16 +29,20 @@ def version():
     info = dict()
     info['software'] = Metadata.TITLE
     info['version'] = get_version()
-    info['keeper-url'] = config.get('keeper-contracts', 'keeper.url')
+    info['keeper-url'] = config.keeper_url
     info['network'] = keeper.get_instance().network_name
     info['contracts'] = dict()
-    info['contracts']['AccessSecretStoreCondition'] = keeper.get_instance().access_secret_store_condition.address
+    info['contracts'][
+        'AccessSecretStoreCondition'] = keeper.get_instance().access_secret_store_condition.address
     info['contracts']['AgreementStoreManager'] = keeper.get_instance().agreement_manager.address
     info['contracts']['ConditionStoreManager'] = keeper.get_instance().condition_manager.address
     info['contracts']['DIDRegistry'] = keeper.get_instance().did_registry.address
     if keeper.get_instance().network_name != 'pacific':
         info['contracts']['Dispenser'] = keeper.get_instance().dispenser.address
-    info['contracts']['EscrowAccessSecretStoreTemplate'] = keeper.get_instance().escrow_access_secretstore_template.address
+    info['contracts'][
+        'EscrowAccessSecretStoreTemplate'] = keeper.get_instance(
+
+    ).escrow_access_secretstore_template.address
     info['contracts']['EscrowReward'] = keeper.get_instance().escrow_reward_condition.address
     info['contracts']['HashLockCondition'] = keeper.get_instance().hash_lock_condition.address
     info['contracts']['LockRewardCondition'] = keeper.get_instance().lock_reward_condition.address
@@ -43,6 +50,7 @@ def version():
     info['contracts']['OceanToken'] = keeper.get_instance().token.address
     info['contracts']['TemplateStoreManager'] = keeper.get_instance().template_manager.address
     info['keeper-version'] = keeper.get_instance().token.version
+    info['provider-address'] = config.parity_address
     return jsonify(info)
 
 
@@ -55,8 +63,6 @@ def spec():
     return jsonify(swag)
 
 
-config = Config(filename=app.config['CONFIG_FILE'])
-brizo_url = config.get(ConfigSections.RESOURCES, 'brizo.url')
 # Call factory function to create our blueprint
 swaggerui_blueprint = get_swaggerui_blueprint(
     BaseURLs.SWAGGER_URL,
