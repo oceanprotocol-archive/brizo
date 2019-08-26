daca02d89dc728d30796c4d689d7e665fc361c0c
ssallam <samer@oceanprotocol.com>
2019-08-26 15:57:35 +0200

    Request tokens for consumer before creating agreement.

--- tests/test_brizo.py
+++ tests/test_brizo.py
@@ -71,6 +71,10 @@ def place_order(publisher_account, service_definition_id, ddo, consumer_account)
     agreement_id = ServiceAgreement.create_new_agreement_id()
     agreement_template = keeper.escrow_access_secretstore_template
     publisher_address = publisher_account.address
+    balance = keeper.token.get_token_balance(consumer_account.address)/(2**18)
+    if balance < 20:
+        keeper.dispenser.request_tokens(100, consumer_account)
+
     service_agreement = ServiceAgreement.from_ddo(service_definition_id, ddo)
     condition_ids = service_agreement.generate_agreement_condition_ids(
         agreement_id, ddo.asset_id, consumer_account.address, publisher_address, keeper)
