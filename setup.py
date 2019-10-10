4cac4104a4ae834097d1e305d36e7884953cfd32
eruizgar91 <enrique@oceanprotocol.com>
2019-10-10 17:02:57 +0200

    Bump version: 0.5.2 → 0.6.0

--- setup.py
+++ setup.py
@@ -86,6 +86,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/oceanprotocol/brizo',
-    version='0.5.2',
+    version='0.6.0',
     zip_safe=False,
 )
