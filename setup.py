e1f3d147e46cdf2fca5b24f6ecc9c779fd5982d8
ssallam <travis@travis-ci.org>
2020-05-14 11:08:31 +0200

    Bump version: 0.9.6 → 0.9.7

--- setup.py
+++ setup.py
@@ -88,6 +88,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/oceanprotocol/brizo',
-    version='0.9.6',
+    version='0.9.7',
     zip_safe=False,
 )
